468151d435ab2be7897919d7719bb38424066add
Vasily Parshentsev <vparshentsev@gmail.com>
2022-11-06 14:15:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(3,7));
+        System.out.println(calc.mult(3,7));
+        System.out.println(calc.pow(3,7));
+    }
+}
